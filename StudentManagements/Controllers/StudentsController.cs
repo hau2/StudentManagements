f20dda74450e39a29761f8cdb7a114afc78b5ad3
hau2 <leconghau095@gmail.com>
2024-10-01 17:04:50 +0700

    195. Getting Started with UI

---
+++ StudentManagements/Controllers/StudentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagements.Controllers
+{
+	public class StudentsController : Controller
+	{
+		[Route("/")]
+		[Route("students/index")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- StudentManagements/Program.cs
+++ StudentManagements/Program.cs
@@ -1,15 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-if(builder.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment())
 {
   app.UseDeveloperExceptionPage();
 }
 
 app.UseStaticFiles();
 app.UseRouting();
-//app.MapControllers();
+app.MapControllers();
 
 app.Run();
