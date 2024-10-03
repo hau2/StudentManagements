28b76d98a40a0b08c3fe1677fa7fb81c5badfb5f
hau2 <leconghau095@gmail.com>
2024-10-03 14:53:45 +0700

    222. Seed Data

--- Entities/StudentsDbContext.cs
+++ Entities/StudentsDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace Entities
 {
@@ -11,6 +12,24 @@ namespace Entities
 			base.OnModelCreating(modelBuilder);
 			modelBuilder.Entity<Classroom>().ToTable("Classrooms");
 			modelBuilder.Entity<Student>().ToTable("Students");
+
+			// Seed to Classrooms
+			string classroomsJson = File.ReadAllText("classrooms.json");
+			List<Classroom> classrooms = JsonSerializer.Deserialize<List<Classroom>>(classroomsJson);
+
+			foreach(Classroom classroom in classrooms)
+			{
+				modelBuilder.Entity<Classroom>().HasData(classroom);
+			}
+
+			// Seed to Students
+			string studentsJson = File.ReadAllText("students.json");
+			List<Student> students = JsonSerializer.Deserialize<List<Student>>(studentsJson);
+
+			foreach (Student student in students)
+			{
+				modelBuilder.Entity<Student>().HasData(student);
+			}
 		}
 	}
 }
